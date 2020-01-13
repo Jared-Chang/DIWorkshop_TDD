3a369e654892b8fd36ebfd52b7eb7733cd6c0cb7
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-01-13 20:28:22 +0800

    refactoring: is_valid

--- AuthenticationServiceTest/AuthenticationServiceTest.cs
+++ AuthenticationServiceTest/AuthenticationServiceTest.cs
@@ -6,12 +6,26 @@ namespace AuthenticationServiceTestNamespace
     [TestFixture]
     public class AuthenticationServiceTest
     {
+        private AuthenticationService _authenticationService;
+        private const string DefaultAccountId = "Account Id";
+        private const string DefaultOtp = "Otp";
+        private const string DefaultPassword = "Password";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _authenticationService = new AuthenticationService();
+        }
+
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            ShouldBeValid(DefaultAccountId, DefaultPassword, DefaultOtp);
+        }
 
-            Assert.AreEqual(true, authenticationService.Verify("Account Id", "Password", "Otp"));
+        private void ShouldBeValid(string accountId, string password, string otp)
+        {
+            Assert.AreEqual(true, _authenticationService.Verify(accountId, password, otp));
         }
     }
 }
