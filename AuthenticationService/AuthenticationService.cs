6927850d86112530a499fa59b5d602aaa8b53dd8
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-01-13 20:44:21 +0800

    refactoring: extract profile dao to field

--- AuthenticationService/AuthenticationService.cs
+++ AuthenticationService/AuthenticationService.cs
@@ -2,10 +2,15 @@ namespace AuthenticationServiceNamespace
 {
     public class AuthenticationService
     {
+        private ProfileDao _profileDao;
+
+        public AuthenticationService()
+        {
+            _profileDao = new ProfileDao();
+        }
+
         public bool Verify(string accountId, string password, string otp)
         {
-            var profileDao = new ProfileDao();
-
             if (password == "Wrong Password" || password == "Wrong Password 2")
                 return false;
             return true;
